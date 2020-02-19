02a13f09406fdf7ba50ca8daa6d175800c345928
suraj <suraj.gtrj@gmail.com>
2020-02-19 20:02:59 +0545

    inegrated rc

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using RingCentral;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            read_user_calllog().Wait();
+        }
+        static private async Task read_user_calllog()
+        {
+            RestClient rc = new RestClient("UJnZe75_RPieDcx9PPL3sQ", "JVoBgGEETSSHJvFQ3l7MLA4LCkpdmcTKeuccWkSgqLDA", false);
+            await rc.Authorize("+19802691329", "101", "deadpool@1");
+            var parameters = new ReadUserCallLogParameters();
+            parameters.view = "Detailed";
+
+            var resp = await rc.Restapi().Account().Extension().CallLog().List(parameters);
+            foreach (var record in resp.records)
+            {
+                Console.WriteLine("Call type: " + record.type);
+                Console.WriteLine("Call type: " + record.to);
+                Console.WriteLine("Call type: " + record.from);
+                Console.WriteLine("Call type: " + record.duration );
+            }
+        }
+    }
+}
